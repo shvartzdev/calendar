b96e6226219f5f36545b3ad62fa6f570a70b9230
Дарья Чернова <dariashvartz@mail.ru>
2018-07-19 15:10:36 +0300

    delete function was added

--- src/store.js
+++ src/store.js
@@ -41,5 +41,14 @@ export const store = {
     return dayObj.events.find(
       event => event.details === eventDetails
     );
+  },
+  deleteEvent(dayId, eventDetails) {
+    const dayObj = this.state.seedData.find(
+      day => day.id === dayId
+    );
+    const eventIndexToRemove = dayObj.events.findIndex(
+      event => event.details === eventDetails
+    );
+    dayObj.events.splice(eventIndexToRemove, 1);
   }
 }
