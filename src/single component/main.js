3e965580a45dbc42c290f2f066197451a1bc1fad
Дарья Чернова <dariashvartz@mail.ru>
2018-07-23 11:01:12 +0300

    vuex integration and refactoring

--- src/single component/main.js
+++ src/single component/main.js
@@ -1,58 +1,87 @@
-const EventBus = new Vue();
-const inputComponent = {
+const state = {
+    notes: [],
+    timestamps: []
+  }
+  
+  const mutations = {
+    ADD_NOTE (state, payload) {
+      let newNote = payload;
+      state.notes.push(newNote);
+    },
+    ADD_TIMESTAMP (state, payload) {
+      let newTimeStamp = payload;
+      state.timestamps.push(newTimeStamp);
+    }
+  }
+  
+  const actions = {
+    addNote (context, payload) {
+      context.commit('ADD_NOTE', payload);
+    },
+    addTimestamp (context, payload) {
+      context.commit('ADD_TIMESTAMP', payload);
+    }
+  }
+  
+  const getters = {
+    getNotes: state => state.notes,
+    getTimestamps: state => state.timestamps,
+    getNoteCount: state => state.notes.length
+  }
+  
+  const store = new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters
+  })
+  
+  const inputComponent = {
     template: `<input
-    :placeholder="placeholder"
-    v-model="input"
-    @keyup.enter="monitorEnterKey"
-    class="input is-small" type="text" />`,
-    props: ['placeholder'],
-    data() {
-        return {
-            input: ''
-        }
+        placeholder='Enter a note'
+        v-model="input"
+        @keyup.enter="monitorEnterKey"
+        class="input is-small" type="text" />`,
+    data () {
+      return {
+        input: '',
+      }
     },
     methods: {
-        monitorEnterKey() {
-            EventBus.$emit('add-note', {
-                note: this.input,
-                timestamp: new Date().toLocaleString()
-            });
-            this.input = '';
-        }
-    }
-  };
-
-  const noteCountComponent = {
-      template: 
-      `<div class="note-count">Note count: <strong>{{ noteCount }}</strong></div>`,
-      data() {
-          return {
-              noteCount: 0
-          }
-      },
-      created() {
-          EventBus.$on('add-note', event => this.noteCount++);
+      monitorEnterKey () {
+        this.$store.dispatch('addNote', this.input);
+        this.$store.dispatch('addTimestamp', new Date().toLocaleString());
+        this.input = '';
       }
+    }
+  }
+  
+  const noteCountComponent = {
+    template:
+      `<div class="note-count">
+        Note count: <strong>{{ noteCount }}</strong>
+      </div>`,
+    computed: {
+      noteCount() {
+        return this.$store.getters.getNoteCount;
+      }
+    }
   }
   
   new Vue({
     el: '#app',
-    data: {
-        notes: [],
-        timestamps: [],
-        placeholder: 'return a note'
-    },
-    methods: {
-        addNote(event) {
-            this.notes.push(event.note);
-            this.timestamps.push(event.timestamp);
-        }
-    },
-    created() {
-        EventBus.$on('add-note', event=>this.addNote(event));
+    store,
+    computed: {
+      notes() {
+        return this.$store.getters.getNotes;
+      },
+      timestamps() {
+        return this.$store.getters.getTimestamps;
+      }
     },
     components: {
       'input-component': inputComponent,
       'note-count-component': noteCountComponent
     }
   })
+  
